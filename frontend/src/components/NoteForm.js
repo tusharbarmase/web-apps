d2bf8a8070a6ed6aec7759fc25ad574208d70a3d
tusharbarmase <tusharbarmase9630@gmail.com>
2022-12-31 13:16:24 +0530

    added form to add notes

---
+++ frontend/src/components/NoteForm.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+const NoteForm = (form) => {
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleClick = () => {
+    document.querySelector(".popup-box").classList.remove("active");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const note = { title, message };
+    const response = await fetch("/api/notes", {
+      method: "POST",
+      body: JSON.stringify(note),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
+    }
+    if (response.ok) {
+      setTitle("");
+      setMessage("");
+      setError(null);
+      handleClick();
+      console.log("added new workout", json);
+    }
+  };
+
+  return (
+    <div className="popup-box">
+      <div className="content">
+        <div className="header">
+          <p>{form.heading}</p>
+          <span className="material-symbols-outlined" onClick={handleClick}>
+            close
+          </span>
+        </div>
+        <form className="create" onSubmit={handleSubmit}>
+          <div className="row title">
+            <label>Title</label>
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+          </div>
+          <div className="row description">
+            <label>Description</label>
+            <textarea
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            ></textarea>
+          </div>
+          <button>{form.button_text}</button>
+          {error && <div className="error">{error}</div>}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NoteForm;
--- frontend/src/pages/Notes.js
+++ frontend/src/pages/Notes.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import NoteDetails from "../components/NoteDetails";
+import NoteForm from "../components/NoteForm";
 import "./Notes.css";
 
 const Notes = () => {
   const [notes, setNotes] = useState(null);
+
+  const handleClick = () => {
+    document.querySelector(".popup-box").classList.add("active");
+  }
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -20,14 +25,14 @@ const Notes = () => {
 
   return (
     <>
+      <NoteForm heading="Add a new Note" button_text="Add Note"/>
       <div className="wrapper">
-        <li className="add-box">
-          <span className="add-icon material-symbols-outlined">add</span>
+        <li className="add-box" onClick={handleClick}>
+          <span className="material-symbols-outlined">add</span>
           <p>Add a new Note</p>
         </li>
-        {notes && notes.map((note) => (
-            <NoteDetails key={note._id} note={note}/>
-          ))}
+        {notes &&
+          notes.map((note) => <NoteDetails key={note._id} note={note} />)}
       </div>
     </>
   );
