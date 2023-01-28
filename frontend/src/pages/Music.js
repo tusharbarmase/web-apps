725418a5855ec052221f510683ed980f7df97740
tusharbarmase <tusharbarmase9630@gmail.com>
2023-01-28 13:20:26 +0530

    music player frontend

--- frontend/src/pages/Music.js
+++ frontend/src/pages/Music.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../styles/Music.css";
 
 const Music = () => {
   return (
-    <div>Music</div>
-  )
-}
+    <div className="player-container">
+      <div className="music-player">
+        <div className="player-header">
+          <span>Sangeet </span>
+          <i className="fa-solid fa-music"></i>
+        </div>
+        <div className="player-image">
+          <img src="https://source.unsplash.com/random" alt="#" />
+        </div>
+        <div className="player-song">
+          <p className="song-name">name</p>
+          <p className="song-artist">artist</p>
+        </div>
+        <div className="player-progress">
+          <input className="progress-bar" type="range" />
+          <div className="progress-timer">
+            <span className="current">0:00</span>
+            <span className="end">3:40</span>
+          </div>
+        </div>
+        <div className="player-controls">
+          <i className="fa-solid fa-repeat"></i>
+          <i className="fa-solid fa-backward-step"></i>
+          <i className="fa-solid fa-play play-pause"></i>
+          <i className="fa-solid fa-forward-step"></i>
+          <i className="fa-solid fa-list"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Music
+export default Music;
